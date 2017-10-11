a322fd24893feeb62ff897deb778682c30ede5c8
Eric Shih <eshih@redhat.com>
2017-10-11 18:12:31 -0500

    try

--- helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
+++ helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
@@ -17,10 +17,13 @@
 package org.jboss.as.quickstarts.html5rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 
 /**
  * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
@@ -34,6 +37,15 @@ public class HelloWorld {
     @Inject
     HelloService helloService;
 
+    @GET
+    @Path("/headers")
+    @Produces("application/json")
+    public String getHelloWorldJSON(@Context HttpHeaders headers) {
+    	
+        return "Headers: " + headers.getRequestHeaders();
+//        return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
+    }
+    
     @POST
     @Path("/json/{name}")
     @Produces("application/json")
