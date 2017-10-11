6dde9a55506f933e403c18e5680474b577a4654a
Eric Shih <eshih@redhat.com>
2017-10-11 18:04:40 -0500

    get headers

--- helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
+++ helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloWorld.java
@@ -17,10 +17,13 @@
 package org.jboss.as.quickstarts.html5rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 
 /**
  * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
@@ -34,13 +37,23 @@ public class HelloWorld {
     @Inject
     HelloService helloService;
 
-    @POST
-    @Path("/json/{name}")
+//    @POST
+//    @Path("/json/{name}")
+//    @Produces("application/json")
+//    public String getHelloWorldJSON(@PathParam("name") String name) {
+//        System.out.println("name: " + name);
+//        return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
+//    }
+    
+    @GET
+    @Path("headers")
     @Produces("application/json")
-    public String getHelloWorldJSON(@PathParam("name") String name) {
-        System.out.println("name: " + name);
-        return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
+    public String getHelloWorldJSON(@Context HttpHeaders headers) {
+    	
+        return "Headers: " + headers.getRequestHeaders();
+//        return "{\"result\":\"" + helloService.createHelloMessage(name) + "\"}";
     }
+    
 
     @POST
     @Path("/xml/{name}")
